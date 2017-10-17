e906d808dc91b93ea876dcbbe47d59448f59bd1c
Yuuki Mori <wrong.int1109@gmail.com>
2017-10-18 02:15:14 +0900

    テスト用コードの削除

--- server.py
+++ server.py
@@ -1,35 +1,12 @@
 # coding: utf-8
 
 from flask import Flask, render_template, request, redirect, url_for, abort
-import simplejson as json
-import datetime
 from werkzeug import secure_filename
 from behavior_extract import BehaviorExtract
 
 
 app = Flask(__name__)
 be = BehaviorExtract()
-
-def read_json(filename):
-    f = open(filename, 'r')
-    jsonData = json.load(f)
-
-    f.close()
-
-    len_processes = len(jsonData['behavior']['processes'])
-    json_dic={}
-    for i in range(0,len_processes):
-        tmp_ctr = len(jsonData['behavior']['processes'][i]['calls'])
-        for j in range(0,tmp_ctr):
-            resizedata = [jsonData['behavior']['processes'][i]['calls'][j]['api'],jsonData['behavior']['processes'][i]['calls'][j]['arguments'],jsonData['behavior']['processes'][i]['calls'][j]['status'],jsonData['behavior']['processes'][i]['calls'][j]['return_value']]
-            time = converttime(jsonData['behavior']['processes'][i]['calls'][j]['time'])
-            json_dic[str(time)]=resizedata
-
-    return json_dic
-
-def converttime(unixtime):
-    time = datetime.datetime.fromtimestamp(unixtime)
-    return time
 
 @app.route('/favicon.ico')
 def favicon():
